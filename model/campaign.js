d4c627e4eabdbd87b9ef81944b90bf3efeb8ea1f
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-02 21:47:51 +0800

    feat: campaign schema

--- model/campaign.js
+++ model/campaign.js
@@ -1,2 +1,46 @@
-import db from './database.js'
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
 
+const campaignSchema = new schema({
+    name: {
+        type: String,
+        required: flase,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "saved", // saved, launched, processing, sent, failed
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    launchDate: {
+        type: Date,
+        required: false,
+    },
+    taGroupId: {
+        type: Array, // change to group ID when segment is set
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    copy: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    landing: {
+        type: String,
+        required: false,
+    },
+});
+const model = mongoose.model("campaign", campaignSchema);
+
+export default model;
