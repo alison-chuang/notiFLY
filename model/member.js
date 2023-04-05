6602d3e34e6c549b1be9d62669b2fbfe9270db6d
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-05 10:57:08 +0800

    feat: create member model

---
+++ model/member.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+    company: {
+        type: String,
+        required: true,
+    },
+    members: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+                unique: true,
+            },
+            cellphone: {
+                type: String,
+                required: false,
+            },
+            created_at: {
+                type: Date,
+                required: true,
+            },
+        },
+    ],
+});
+
+const Member = mongoose.model("member", memberSchema);
+
+export { Member };
