c868da81e43804a6bfeb520051a2354688c96175
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-10 02:03:27 +0800

    feat: set-up router

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 const { PORT, API_VERSION } = process.env;
 
 // Express Initialization
-import express from 'express';
+import express from "express";
 const app = express();
-const port = PORT
+const port = PORT;
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Altas database connection 
-import db from './model/database.js'
+// Altas database connection
+import "./model/database.js";
 
 // API routes
-import campaignRouter from './routes/campaign.js'
-app.use('/api/' + API_VERSION, campaignRouter);
+import campaignRouter from "./routes/campaign.js";
+app.use("/api/" + API_VERSION, campaignRouter);
 
 // Page not found
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/public/404.html');
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 // Error handling
 app.use((err, req, res, next) => {
     console.log(err);
-    res.status(500).json('Internal Server Error');
+    res.status(500).json("Internal Server Error");
 });
 
 app.listen(port, () => {
     console.log(`The application is runnung on localhost ${port}`);
 });
-
