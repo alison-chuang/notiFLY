142d537fd4956b382870820c55474190ad3a0db7
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-10 02:10:50 +0800

    feat: campaign route

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,8 +1,18 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
+import { wrapAsync } from "../util/util.js";
+import { postCampaigns, getS3Url, lambdaUpdateDb, getSegmentNames, getS3Images } from "../controller/campaign.js";
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
-router.get('/campaigns', (req, res) => {
-    res.send('Campaigns');
-});
+router.route("/campaigns").post(wrapAsync(postCampaigns));
+
+router.route("/segments").get(wrapAsync(getSegmentNames));
+
+router.route("/images").get(wrapAsync(getS3Images));
+
+router.route("/s3Url").get(wrapAsync(getS3Url));
+
+router.route("/lambdaUpdateDb").post(wrapAsync(lambdaUpdateDb));
 
 export default router;
