edb1547bbfd5f934c0a0f16f9e272075f0df042e
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-10 02:12:03 +0800

    feat: util for file naming

---
+++ util/util.js
@@ -0,0 +1,15 @@
+// for s3 file name
+import crypto from "crypto";
+import { promisify } from "util";
+const randomBytes = promisify(crypto.randomBytes);
+
+// for error handler
+const wrapAsync = (fn) => {
+    return function (req, res, next) {
+        // Make sure to `.catch()` any errors and pass them along to the `next()`
+        // middleware in the chain, in this case the error handler.
+        fn(req, res, next).catch(next);
+    };
+};
+
+export { wrapAsync, randomBytes };
