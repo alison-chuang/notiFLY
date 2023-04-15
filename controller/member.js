8b6163d2058fb7aa573b49c1c5afe047d6ac248c
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-15 13:03:18 +0800

    feat: endpoint for pushing client data

--- controller/member.js
+++ controller/member.js
@@ -1,10 +1,12 @@
 import Ajv from "ajv";
-import { Member, newAttribute, newOrder, deleteOrder, checkMemberId } from "../model/member.js";
+import { Member, newAttribute, newOrder, delOrder, checkMemberId } from "../model/member.js";
 import { newMemberSchema } from "../util/util.js";
 
 // save campaign info to db
 const postMember = async (req, res) => {
     console.log("client push new members:", req.body);
+    const { body } = req;
+    const { email, cellphone } = req.body;
 
     // validate body format
     const ajv = new Ajv();
@@ -16,7 +18,7 @@ const postMember = async (req, res) => {
 
     // required fields
     if (!email || !cellphone) {
-        return res.status(400).json({ data: "email is required" });
+        return res.status(400).json({ data: "email and cellphone is required" });
     }
     // validate email, cellphone
     const tester = (regex, field) => {
@@ -24,20 +26,20 @@ const postMember = async (req, res) => {
             return res.status(400).send(`Invalid ${field}`);
         }
     };
-    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+$/g;
+    const emailRegex = /^[\w.+-]+@(?:[a-z\d-]+\.)+[a-z]{2,}$/g;
     const cellphoneRegex = /^([0-9]{4})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{3})$/;
-    tester(email, emailRegex);
-    tester(cellphone, cellphoneRegex);
+    tester(emailRegex, email);
+    tester(cellphoneRegex, cellphone);
 
     // save to DB
     try {
-        const member = new Member(data);
+        const member = new Member(body);
         const newmember = await member.save();
         console.log("saved member to database");
-        res.status(201).json(newmember);
+        return res.status(201).json(newmember);
     } catch (e) {
         console.log(e);
-        res.status(500).json({ data: e.message });
+        return res.status(500).json({ data: e.message });
     }
 };
 
@@ -45,63 +47,63 @@ const postMember = async (req, res) => {
 const updateMember = async (req, res) => {
     console.log("client update member data:", req.body);
 
-    const [id] = req.body;
-    const [body] = req;
+    const { id } = req.body;
+    const { body } = req;
 
     // check member in db
     const isMember = await checkMemberId(id);
     if (!isMember) {
-        res.status(400).json({ data: "bad request" });
+        return es.status(400).json({ data: "bad request" });
     }
 
     // $set: object
     try {
         const updated = await newAttribute(id, body);
-        console.log("updated doc:", updated);
-        res.status(200).json({ data: "DB updated" });
+        console.log("updated newAttribute:", updated);
+        return res.status(200).json({ data: "DB updated" });
     } catch (e) {
         console.error(e);
-        res.status(500).json({ data: "fail to update" });
+        return res.status(500).json({ data: "fail to update" });
     }
 };
 
 const updateOrder = async (req, res) => {
+    // console.log("client update member data:", req.body);
+    const { id, order } = req.body;
+
+    // check member in db
+    const isMember = await checkMemberId(id);
+    if (!isMember) {
+        return res.status(400).json({ data: "bad request" });
+    }
+
+    // $push : new order
+    try {
+        const updated = await newOrder(id, order);
+        return res.status(200).json({ data: "DB updated" });
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ data: "fail to update" });
+    }
+};
+
+const deleteOrder = async (req, res) => {
     console.log("client update member data:", req.body);
     const { id, order } = req.body;
 
     // check member in db
     const isMember = await checkMemberId(id);
     if (!isMember) {
-        res.status(400).json({ data: "bad request" });
-    }
-
-    // $push : new order
-    try {
-        const updated = await newOrder(id, order);
-        res.status(200).json({ data: "DB updated" });
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ data: "fail to update" });
-    }
-};
-
-const deleteOrder = async (req, res) => {
-    console.log("client update member data:", req.body);
-    const { id, orders } = req.body;
-
-    // check member in db
-    const isMember = await checkMemberId(id);
-    if (!isMember) {
-        res.status(400).json({ data: "bad request" });
+        return res.status(400).json({ data: "bad request" });
     }
 
     // $: delete order
     try {
-        const updated = await deleteOrder(id, body);
-        res.status(200).json({ data: "DB updated" });
+        const updated = await delOrder(id, order);
+        return res.status(200).json({ data: "DB updated" });
     } catch (e) {
         console.error(e);
-        res.status(500).json({ data: "fail to delete" });
+        return res.status(500).json({ data: "fail to delete" });
     }
 };
 
--- routes/member.js
+++ routes/member.js
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { wrapAsync } from "../util/util.js";
-import { postMember } from "../controller/campaign.js";
+import { postMember, updateMember, updateOrder, deleteOrder } from "../controller/member.js";
 router.use(express.json());
 
 router.route("/members").post(wrapAsync(postMember));
-router.route("/members").post(wrapAsync(updateMember));
+router.route("/members").put(wrapAsync(updateMember));
+router.route("/members/order").post(wrapAsync(updateOrder));
+router.route("/members/order").put(wrapAsync(deleteOrder));
+
+export default router;
