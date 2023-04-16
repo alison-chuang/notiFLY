72e50cbd984821c7464fc84236adba0c9aeced78
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-16 09:50:40 +0800

    fix: save rules for re-render query rule

--- model/segment.js
+++ model/segment.js
@@ -13,6 +13,9 @@ const segmentSchema = new Schema({
     query: {
         type: Object,
     },
+    rules: {
+        type: Object,
+    },
     created_at: {
         type: Date,
         required: true,
@@ -35,9 +38,9 @@ const selectAllSegment = async () => {
 };
 
 // TODO update segment (放在編輯頁的 save button)
-const updateSegment = async (newQuery) => {
+const updateSegment = async (id, query, name, rules) => {
     const filter = { _id: id };
-    const update = { $set: { query: newQuery } };
+    const update = { $set: { query: query, name: name, rules: rules } };
     const doc = await Segment.findOneAndUpdate(filter, update, {
         new: true,
     });
@@ -45,4 +48,14 @@ const updateSegment = async (newQuery) => {
     return doc;
 };
 
-export { Segment, selectSegmentNames, updateSegment, selectAllSegment };
+// TODO select segment by id
+const selectById = async (id) => {
+    try {
+        const doc = await Segment.findOne({ _id: id }, { query: 0 });
+        return doc;
+    } catch (e) {
+        return false;
+    }
+};
+
+export { Segment, selectSegmentNames, updateSegment, selectAllSegment, selectById };
