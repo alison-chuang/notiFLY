6552952d9fb446d7d1c5130c051c065e6c34cb83
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-22 01:23:20 +0800

    conf: list const in single file

---
+++ statusConstant.js
@@ -0,0 +1,10 @@
+// campaign
+export const REGISTERED = "registered";
+export const RUNNING = "running";
+export const STOPPED = "stopped";
+
+// job
+export const LAUNCHED = "launched";
+export const PROCESSING = "processing";
+export const PROCESSED = "processed";
+export const FAILED = "failed";
