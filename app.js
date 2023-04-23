698db0750fac26c9ea8b871f9648bc59c6edc4c4
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-23 10:08:56 +0800

    fix: adjust reset password our of static folder

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
