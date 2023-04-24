69ff466ac229c2810f60693a3e1f96ec9f7d5070
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-04-24 16:38:28 +0800

    feat: sign-in frontend js

---
+++ public/script/signin.js
@@ -0,0 +1,34 @@
+const jwtToken = localStorage.getItem("jwtToken");
+if (jwtToken && jwtToken !== "undefined") {
+    window.location.replace("/document.html");
+}
+
+$(document).ready(function () {
+    $("#signInBtn").on("click", async (e) => {
+        const email = $("#signInForm input[name=email]").val();
+        const password = $("#signInForm input[name=password]").val();
+        e.preventDefault();
+
+        const body = {
+            email: email,
+            password: password,
+        };
+        $("#signInBtn").prop("disabled", true);
+        try {
+            const response = await axios.post("/api/1.0/users/signin", body);
+            const token = response.data.data.access_token;
+            localStorage.setItem("jwtToken", token);
+            window.location.replace("/document.html");
+        } catch (e) {
+            console.error("error", e);
+            Swal.fire({
+                icon: "error",
+                title: `Error!`,
+                text: `Sign in failed:  ${e.response.data.data}`,
+                confirmButtonColor: "#F27475",
+                allowOutsideClick: false,
+            });
+            $("#signInBtn").prop("disabled", false);
+        }
+    });
+});
