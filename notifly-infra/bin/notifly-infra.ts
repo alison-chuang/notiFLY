6dae3063071aa08cc5d77390011326385b868e68
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2023-05-18 01:58:31 +0800

    feat: prepare lambda dependency when building

--- notifly-infra/bin/notifly-infra.ts
+++ notifly-infra/bin/notifly-infra.ts
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
+import { exec } from "child_process";
+import * as path from "path";
+
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { NotiflyInfraStack } from "../lib/notifly-infra-stack";
 
 const app = new cdk.App();
+
+function prepareLambdaDependency() {
+    exec("npm i", { cwd: `${path.join(__dirname, "..")}/asset/edm` });
+    exec("npm i", { cwd: `${path.join(__dirname, "..")}/asset/web-push` });
+}
+
+prepareLambdaDependency();
 new NotiflyInfraStack(app, "NotiflyInfraStack", {
     /* If you don't specify 'env', this stack will be environment-agnostic.
      * Account/Region-dependent features and context lookups will not work,
